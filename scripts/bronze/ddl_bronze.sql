0c7b85f9222b3fb9082db039422087eda51c9216
SatyajitDubal123 <satyajitdubal@gmail.com>
2026-05-01 11:58:53 +0530

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,61 @@
+/*
+DDL Script: Create Bronze Tables.
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables
+    if they already exists.
+    Run this scripts to re-define the DDL structure of bronze tables.
+*/
+
+
+create table bronze.crm_cust_info(
+		cst_id int,
+		cst_key NVARCHAR(50),
+		cst_firstname nvarchar(50),
+		cst_lastname nvarchar(50),
+		cst_marital_status nvarchar(50),
+		cst_gndr nvarchar(50),
+		cst_create_date date
+);
+
+create table bronze.crm_prd_info(
+		prd_id int,
+		prd_key NVARCHAR(50),
+		prd_nm NVARCHAR(50),
+		prd_cost int,
+		prd_line NVARCHAR(50),
+		prd_start_dt DATETIME,
+		prd_end_dt DATETIME
+);
+
+create table bronze.crm_sales_details(
+		sls_ord_num NVARCHAR(50),
+		sls_prd_key NVARCHAR(50),
+		sls_cust_id INT,
+		sls_order_dt int,
+		sls_ship_dt INT,
+		sls_due_dt int,
+		sls_sales int,
+		sls_quantity int,
+		sls_price int
+)
+
+create table bronze.erp_loc_a101(
+		cid nvarchar(50),
+		cntry nvarchar(50)
+);
+go
+
+create table bronze.erp_cust_az12(
+		cid nvarchar(50),
+		bdate date,
+		gen nvarchar(50)
+);
+go
+
+create table bronze.erp_px_cat_g1v2(
+		id nvarchar(50),
+		cat nvarchar(50),
+		subcat nvarchar(50),
+		maintenance nvarchar(50)
+);
+
